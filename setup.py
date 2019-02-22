b5d28ccec4b254adc14e5c7b3f02b32c78b6f447
Moritz Moeller <mm@mxs.de>
2019-02-22 08:52:22 +0100

    ..

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 # -*- coding: utf8 -*-
 from setuptools import setup
 setup(
-    name        = 'control',
-    packages    = ['control'],
-    package_dir = { 'control': '.' },
-    scripts     = ['control'],
-    version     = '0.1',
-    description = 'manage processes',
-    author      = 'Moritz Möller',
-    author_email= 'mm@mxs.de',
-    url         = 'https://github.com/mo22/control',
+    name         = 'control',
+    packages     = ['control'],
+    package_dir  = { 'control': '.' },
+    scripts      = ['control'],
+    version      = '0.2',
+    description  = 'manage processes',
+    author       = 'Moritz Moeller',
+    author_email = 'mm@mxs.de',
+    url          = 'https://github.com/mo22/control',
     install_requires = [
         'pyyaml',
         'jsonschema',
