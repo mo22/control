dc81044df28cbaf066eb5261a8f6f3e2ccb60290
Moritz Moeller <mm@mxs.de>
2019-02-22 10:26:10 +0100

    ..

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ from setuptools import setup
 setup(
     name         = 'control',
     packages     = ['control'],
-    package_dir  = { 'control': '.' },
-    scripts      = ['control'],
+    #package_dir  = { 'control': '.' },
+    #scripts      = ['control'],
     version      = '0.2',
     description  = 'manage processes',
     author       = 'Moritz Moeller',
     author_email = 'mm@mxs.de',
     url          = 'https://github.com/mo22/control',
+    #zip_safe     = False,
     install_requires = [
         'pyyaml',
         'jsonschema',
     ],
 )
-
